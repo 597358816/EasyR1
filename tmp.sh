10d85a72a3d00e1e790f4999b3ac1c275a3914b9
chenwang <597358816@qq.com>
2026-06-07 07:42:34 +0000

    before OPD

--- tmp.sh
+++ tmp.sh
@@ -4,9 +4,8 @@
 #     python3 /home/dataset-assist-0/wc/EasyR1/scripts/model_merger.py --local_dir /home/dataset-assist-0/wc/checkpoints/Qwen2.5-3B/qwen2.5-3b-Ins-head/global_step_${step}/actor/
 # done
 
-python3 /home/dataset-assist-0/wc/EasyR1/scripts/model_merger.py --local_dir /home/dataset-assist-0/wc/checkpoints/Qwen2.5-7B/qwen2.5-7b-CISPO/global_step_20/actor/
-python3 /home/dataset-assist-0/wc/EasyR1/scripts/model_merger.py --local_dir /home/dataset-assist-0/wc/checkpoints/Qwen2.5-7B/qwen2.5-7b-CISPO/global_step_40/actor/
-python3 /home/dataset-assist-0/wc/EasyR1/scripts/model_merger.py --local_dir /home/dataset-assist-0/wc/checkpoints/Qwen2.5-7B/qwen2.5-7b-CISPO/global_step_60/actor/
-python3 /home/dataset-assist-0/wc/EasyR1/scripts/model_merger.py --local_dir /home/dataset-assist-0/wc/checkpoints/Qwen2.5-7B/qwen2.5-7b-CISPO/global_step_80/actor/
+
+python3 /vepfs-mlp2/c20250203/250602012/EasyR1/scripts/model_merger.py --local_dir /vepfs-mlp2/c20250203/250602012/checkpoints/Qwen3-4B/qwen3-4b-GRPO2/global_step_139/actor
+python3 /vepfs-mlp2/c20250203/250602012/EasyR1/scripts/model_merger.py --local_dir /vepfs-mlp2/c20250203/250602012/checkpoints/Qwen3-8B/qwen3-8b-GRPO/global_step_139/actor
 
 
